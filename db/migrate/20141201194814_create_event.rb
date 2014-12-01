8180ded154d29bd2708a1206e0cd1e2c3eadc09c
Michaeldean147 <michaeldean147@yahoo.com>
2014-12-01 13:35:36 -0800

    initial commit

---
+++ db/migrate/20141201194814_create_event.rb
@@ -0,0 +1,11 @@
+class CreateEvent < ActiveRecord::Migration
+  def change
+    create_table :events do |t|
+      t.date :date
+      t.string :description
+      t.boolean :requires_id, :default => false, :null => false
+      t.integer :capacity
+      t.text :directions
+    end
+  end
+end
